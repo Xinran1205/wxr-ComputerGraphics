3e67aa352d6270cfc9edf6fafb359654dcbfea54
Xinran1205 <id19037@bristol.ac.uk>
2023-10-02 14:59:18 +0100

    cg lab2 task 1-5 done

---
+++ lab1&lab2/RedNoise/src/RedNoise.cpp
@@ -0,0 +1,108 @@
+#include <CanvasTriangle.h>
+#include <DrawingWindow.h>
+#include <Utils.h>
+#include "glm/glm.hpp"
+
+#define WIDTH 320
+#define HEIGHT 240
+
+std::vector<float> interpolateSingleFloats(float from,float to, int numberOfValues);
+std::vector<glm::vec3> interpolateTripleFloats(glm::vec3 from, glm::vec3 to, int numberOfValues);
+
+
+// Task 2: Single Element Numerical Interpolation
+// return a list of floats that are interpolated between the two floats
+std::vector<float> interpolateSingleFloats(float from,float to, int numberOfValues){
+    int gap = numberOfValues - 1;
+    float step = (to - from) / gap;
+    std::vector<float> result;
+    for (int i = 0; i < numberOfValues; i++){
+        result.push_back(from + (step * i));
+    }
+    return result;
+}
+
+//Task 3: Single Dimension Greyscale Interpolation
+void drawTheGreyScale(DrawingWindow &window) {
+    window.clearPixels();
+    for (size_t y = 0; y < window.height; y++) {
+        // Interpolate the grey values for this row.
+        std::vector<float> greyValues = interpolateSingleFloats(255.0, 0.0, window.width);
+        for (size_t x = 0; x < window.width; x++) {
+            // Use the interpolated grey value for R, G, and B channels.
+            float grey = greyValues[x];
+            // use the Alpha channel to make the image transparent.
+            uint32_t colour = (255 << 24) + (int(grey) << 16) + (int(grey) << 8) + int(grey);
+            window.setPixelColour(x, y, colour);
+        }
+    }
+}
+
+// Task 4: Three Element Numerical Interpolation
+//return a list of 3D vectors that are interpolated between the two 3D vectors
+std::vector<glm::vec3> interpolateTripleFloats(glm::vec3 from, glm::vec3 to, int numberOfValues){
+    int gap = numberOfValues - 1;
+    float stepX = (to.x - from.x) / gap;
+    float stepY = (to.y - from.y) / gap;
+    float stepZ = (to.z - from.z) / gap;
+    std::vector<glm::vec3> result;
+    for (int i = 0; i < numberOfValues; i++){
+        result.push_back(glm::vec3(from.x + (stepX * i), from.y + (stepY * i), from.z + (stepZ * i)));
+    }
+    return result;
+}
+
+//Task 5: Two Dimensional Colour Interpolation
+void draw(DrawingWindow &window) {
+    window.clearPixels();
+    glm::vec3 topLeft(255, 0, 0);        // red
+    glm::vec3 topRight(0, 0, 255);       // blue
+    glm::vec3 bottomRight(0, 255, 0);    // green
+    glm::vec3 bottomLeft(255, 255, 0);   // yellow
+    std::vector<glm::vec3> leftSide = interpolateTripleFloats(topLeft, bottomLeft, window.height);
+    std::vector<glm::vec3> rightSide = interpolateTripleFloats(topRight, bottomRight, window.height);
+    for (size_t y = 0; y < window.height; y++) {
+        std::vector<glm::vec3> row = interpolateTripleFloats(leftSide[y], rightSide[y], window.width);
+        for (size_t x = 0; x < window.width; x++) {
+            glm::vec3 colour = row[x];
+            uint32_t colourInt = (255 << 24) + (int(colour.x) << 16) + (int(colour.y) << 8) + int(colour.z);
+            window.setPixelColour(x, y, colourInt);
+        }
+    }
+}
+
+void handleEvent(SDL_Event event, DrawingWindow &window) {
+	if (event.type == SDL_KEYDOWN) {
+		if (event.key.keysym.sym == SDLK_LEFT) std::cout << "LEFT" << std::endl;
+		else if (event.key.keysym.sym == SDLK_RIGHT) std::cout << "RIGHT" << std::endl;
+		else if (event.key.keysym.sym == SDLK_UP) std::cout << "UP" << std::endl;
+		else if (event.key.keysym.sym == SDLK_DOWN) std::cout << "DOWN" << std::endl;
+	} else if (event.type == SDL_MOUSEBUTTONDOWN) {
+		window.savePPM("output.ppm");
+		window.saveBMP("output.bmp");
+	}
+}
+
+int main(int argc, char *argv[]) {
+//    // test interpolateSingleFloats
+//    std::vector<float> result;
+//    result = interpolateSingleFloats(2.2, 8.5, 7);
+//    for(size_t i=0; i<result.size(); i++) std::cout << result[i] << " ";
+//    std::cout << std::endl;
+
+    // test interpolateTripleFloats
+//    std::vector<glm::vec3> result2;
+//    result2 = interpolateTripleFloats(glm::vec3(1.0, 4.0, 9.2), glm::vec3(4.0, 1.0, 9.8), 4);
+//    for(size_t i=0; i<result2.size(); i++) std::cout << result2[i].x << " " << result2[i].y << " " << result2[i].z << std::endl;
+//    std::cout << std::endl;
+
+	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
+	SDL_Event event;
+	while (true) {
+		// We MUST poll for events - otherwise the window will freeze !
+		if (window.pollForInputEvents(event)) handleEvent(event, window);
+		draw(window);
+		// Need to render the frame at the end, or nothing actually gets shown on the screen !
+		window.renderFrame();
+	}
+}
