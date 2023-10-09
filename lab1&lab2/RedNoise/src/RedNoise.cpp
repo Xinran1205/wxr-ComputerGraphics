686b6514d76cfc3734cd4cbdeb9e43f57c50816a
Xinran1205 <id19037@bristol.ac.uk>
2023-10-09 14:49:25 +0100

    task5 finished, texture done!

--- lab1&lab2/RedNoise/src/RedNoise.cpp
+++ lab1&lab2/RedNoise/src/RedNoise.cpp
@@ -5,6 +5,7 @@
 #include "CanvasPoint.h"
 #include <Colour.h>
 #include <CanvasTriangle.h>
+#include <TextureMap.h>
 
 #define WIDTH 320
 #define HEIGHT 240
@@ -12,7 +13,7 @@
 std::vector<float> interpolateSingleFloats(float from,float to, int numberOfValues);
 std::vector<glm::vec3> interpolateTripleFloats(glm::vec3 from, glm::vec3 to, int numberOfValues);
 void drawLineBresenham(DrawingWindow &window, CanvasPoint from, CanvasPoint to, Colour colour);
-
+std::vector<TexturePoint> interpolateTexturePoints(TexturePoint start, TexturePoint end, int numValues);
 // drawLine by using Bresenham's line algorithm
 void drawLineBresenham(DrawingWindow &window, CanvasPoint from, CanvasPoint to, Colour colour){
     // we use absolute value to make sure the slope is always positive
@@ -256,6 +257,81 @@ void drawFilledTriangle (DrawingWindow &window, CanvasTriangle triangle, Colour 
     }
 }
 
+void drawTextureTriangle(DrawingWindow &window, CanvasTriangle triangle,TextureMap &textureMap) {
+    CanvasPoint bottom = triangle[0];
+    CanvasPoint middle = triangle[1];
+    CanvasPoint top = triangle[2];
+
+    // sort the points by y value
+    if (bottom.y > middle.y) {
+        std::swap(bottom, middle);
+    }
+    if (bottom.y > top.y) {
+        std::swap(bottom, top);
+    }
+    if (middle.y > top.y) {
+        std::swap(middle, top);
+    }
+
+    std::vector<float> xValuesBottomToTop = interpolateSingleFloats(bottom.x, top.x, top.y - bottom.y + 1);
+    std::vector<float> xValuesBottomToMiddle = interpolateSingleFloats(bottom.x, middle.x, middle.y - bottom.y + 1);
+    std::vector<float> xValuesMiddleToTop = interpolateSingleFloats(middle.x, top.x, top.y - middle.y + 1);
+
+    std::vector<TexturePoint> texCoordsBottomToTop = interpolateTexturePoints(bottom.texturePoint, top.texturePoint, top.y - bottom.y + 1);
+    std::vector<TexturePoint> texCoordsBottomToMiddle = interpolateTexturePoints(bottom.texturePoint, middle.texturePoint, middle.y - bottom.y + 1);
+    std::vector<TexturePoint> texCoordsMiddleToTop = interpolateTexturePoints(middle.texturePoint, top.texturePoint, top.y - middle.y + 1);
+
+    // Rasterize top half triangle (from middle to top)
+    for (int i = 0; i < top.y - middle.y + 1; i++) {
+        int y = middle.y + i;
+        int x_start = xValuesMiddleToTop[i];
+        int x_end = xValuesBottomToTop[middle.y - bottom.y + i];
+        TexturePoint texStart = texCoordsMiddleToTop[i];
+        TexturePoint texEnd = texCoordsBottomToTop[middle.y - bottom.y + i];
+        std::vector<TexturePoint> texRow = interpolateTexturePoints(texStart, texEnd, x_end - x_start + 1);
+
+        int texIdx = 0;
+        for (int x = x_start; x <= x_end; x++) {
+            if (x >= 0 && (size_t)x < window.width && (size_t)y >= 0 && (size_t)y < window.height) {
+                uint32_t colour = textureMap.pixels[(size_t)texRow[texIdx].y * textureMap.width + (size_t)texRow[texIdx].x];
+                window.setPixelColour(x, y, colour);
+            }
+            texIdx++;
+        }
+    }
+
+    // Rasterize bottom half triangle (from bottom to middle)
+    for (int i = 0; i < middle.y - bottom.y + 1; i++) {
+        int y = bottom.y + i;
+        int x_start = xValuesBottomToMiddle[i];
+        int x_end = xValuesBottomToTop[i];
+        TexturePoint texStart = texCoordsBottomToMiddle[i];
+        TexturePoint texEnd = texCoordsBottomToTop[i];
+        std::vector<TexturePoint> texRow = interpolateTexturePoints(texStart, texEnd, x_end - x_start + 1);
+
+        int texIdx = 0;
+        for (int x = x_start; x <= x_end; x++) {
+            if (x >= 0 && (size_t)x < window.width && (size_t)y >= 0 && (size_t)y < window.height) {
+                uint32_t colour = textureMap.pixels[(size_t)texRow[texIdx].y * textureMap.width + (size_t)texRow[texIdx].x];
+                window.setPixelColour(x, y, colour);
+            }
+            texIdx++;
+        }
+    }
+}
+
+std::vector<TexturePoint> interpolateTexturePoints(TexturePoint start, TexturePoint end, int numValues) {
+    std::vector<TexturePoint> result;
+    float dx = (end.x - start.x) /(numValues - 1);
+    float dy = (end.y - start.y) /(numValues - 1);
+
+    for (int i = 0; i < numValues; i++) {
+        result.push_back(TexturePoint(start.x + i * dx, start.y + i * dy));
+    }
+    return result;
+}
+
+
 // Task 2: Single Element Numerical Interpolation
 // return a list of floats that are interpolated between the two floats
 std::vector<float> interpolateSingleFloats(float from,float to, int numberOfValues){
@@ -346,6 +422,19 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 
             CanvasTriangle randomTriangle(p1, p2, p3);
             drawFilledTriangle(window, randomTriangle, Colour(rand()%255, rand()%255, rand()%255));
+        } else if (event.key.keysym.sym == SDLK_3) {
+            //texture map
+            std::cout << "3 is pressed, I dont know how to set this to t" << std::endl;
+            CanvasPoint p1(160, 10);
+            p1.texturePoint = TexturePoint(195, 5);
+            CanvasPoint p2(300, 230);
+            p2.texturePoint = TexturePoint(395, 380);
+            CanvasPoint p3(10, 150);
+            p3.texturePoint = TexturePoint(65, 330);
+
+            CanvasTriangle triangle(p1, p2, p3);
+            TextureMap textureMap("../texture.ppm");
+            drawTextureTriangle(window, triangle, textureMap);
         }
 	} else if (event.type == SDL_MOUSEBUTTONDOWN) {
 		window.savePPM("output.ppm");
